7c1ba6bcc4c10f218b1f0887d9c5e85d61647c1b
catarina_jesus <catarinaqj@gmail.com>
2025-11-25 20:00:03 +0100

    Mummy canvas for outro

--- Assets/VRTemplateAssets/Scripts/SceneManager.cs
+++ Assets/VRTemplateAssets/Scripts/SceneManager.cs
@@ -1,36 +1,60 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using Unity.Android.Gradle.Manifest;
 
 public class SceneManager : MonoBehaviour
 {
     [SerializeField] private CanvasGroup outroPanel;
     [SerializeField] private float fadeDuration = 3f;
 
+    [SerializeField] private CanvasGroup mummyCanvas;
+
+    [SerializeField] private Button restartButton;
+
 
     void Start()
     {
-      StartCoroutine(StartOutro(outroPanel));
+      StartCoroutine(StartOutro(mummyCanvas, outroPanel));
+      mummyCanvas.enabled = false;
    
     }
 
-    IEnumerator StartOutro(CanvasGroup group)
+    IEnumerator StartOutro(CanvasGroup mummy,CanvasGroup outro)
     {
         yield return new WaitForSeconds(2); 
-        yield return StartCoroutine(Fade(group, 3f)); 
+        yield return StartCoroutine(Fade(outro, 3f)); 
+        mummyCanvas.enabled = true;
+        yield return new WaitForSeconds(3f);
+        mummyCanvas.enabled = true;
+        outroPanel.enabled = true;
+
     }
 
     
     IEnumerator Fade(CanvasGroup group, float targetAlpha)
-{
-    float startAlpha = group.alpha;
-    float time = 0;
-    while (time < fadeDuration)
     {
-        time += Time.deltaTime;
-        group.alpha = Mathf.Lerp(startAlpha, targetAlpha, time / fadeDuration);
-        yield return null; 
+        float startAlpha = group.alpha;
+        float time = 0;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            group.alpha = Mathf.Lerp(startAlpha, targetAlpha, time / fadeDuration);
+            yield return null; 
+        }
+        group.alpha = targetAlpha;
     }
-    group.alpha = targetAlpha;
-}
+
+    void OllisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "RestartButton")
+        {
+            RestartScene();
+        }
+        
+    }
+    void RestartScene()
+    {
+       UnityEngine.SceneManagement.SceneManager.LoadScene("IntroScene");
+    }
 }
